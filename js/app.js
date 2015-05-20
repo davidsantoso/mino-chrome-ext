75ec1d8f76e5809c04e510fc19d76441b1afb070
David Santoso <david.e.santoso@gmail.com>
2015-05-19 22:11:34 -0400

    Some useful generators

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.route('index', { path: '/' }); // Not needed but I like the explicitness!
+  this.route('login')
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -9,3 +10,17 @@ App.IndexRoute = Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   }
 });
+
+App.LoginRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
+
+App.LoginController = Ember.Controller.extend({
+  actions: {
+    login: function() {
+      console.log("Clicked Login!")
+    }
+  }
+})
